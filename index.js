66210805887ff15b8f04ada84af2bc6c8f18fb43
Marnei Machado <marnei.machado@gmail.com>
2022-05-15 20:09:58 -0300

    post adicionado

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 //fake posts
 let posts = [
   {
-    id: "ladksjflk",
+    id: "1234",
     title: "Teste do mural",
     description: "Descrição de teste",
   },
@@ -18,8 +18,19 @@ app.get("/all", (req, res) => {
   res.json(JSON.stringify(posts));
 });
 
-app.post("/new", (req, res) => {});
+app.post("/new", bodyParser.json(), (req, res) => {
+  let id = generateID();
+  let title = req.body.title;
+  let description = req.body.description;
+  posts.push({ id: id, title: title, description: description });
+
+  res.send("Post created");
+});
 
 app.listen(PORT, () => {
   console.log("server is running on:", PORT);
 });
+
+function generateID() {
+  return Math.random().toString(36).substring(2, 9);
+}
